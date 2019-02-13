c81f6bf46b1d6b028a57d6477436dbddcc2d2293
Weston Steimel <weston.steimel@gmail.com>
2019-02-13 20:48:22 +0000

    add docker and run everything via docker
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: serve deploy
+.PHONY: serve deploy test shellcheck
 
 help: ## Show this help
 	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//'
@@ -7,4 +7,19 @@ deploy: ## Deploy to GitHub Pages
 	@$(CURDIR)/deploy.sh
 
 serve: ## Serve a local development copy
-	hugo serve -D
+	docker run --rm -i $(DOCKER_FLAGS) \
+    	--net host \
+		-v $(CURDIR):/site \
+		-p 1313:1313 \
+    	--workdir /site \
+    	--name hugo-website-serve \
+    	"westonsteimel/hugo" serve -D
+
+test: shellcheck serve
+
+shellcheck: ## Runs the shellcheck tests on the scripts.
+	docker run --rm -i $(DOCKER_FLAGS) \
+        --name website-shellcheck \
+        -v $(CURDIR):/usr/src:ro \
+        --workdir "/usr/src" \
+        "westonsteimel/shellcheck:alpine" ./shellcheck.sh
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to westonsteimel.github.io...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
+# Build the project using hugo in docker.
+docker run --rm \
+    -v "$PWD:/site" \
+    --workdir /site \
+    --name hugo-website-builder \
+    westonsteimel/hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+(
 # Go To Public folder
-cd public
+cd public || exit
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuilding site $(date)"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -19,6 +25,4 @@ git commit -s -m "$msg"
 
 # Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
-cd ..
+)
